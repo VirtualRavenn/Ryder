f75aa90b5cf7e6ab286c993581f6f9d0099d3c07
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-26 13:05:05 +1100

    Update index.js

--- index.js
+++ index.js
@@ -13,4 +13,8 @@ client.on("message", message => {
   if (message.author.id === "416418286184431636") message.reply("Watch the road, **motherfucker**, the road!");
 });
 
+client.on("message", message => {
+  if (message.author.id === "417500499424706560") message.reply("Oh man, we gonna die!");
+});
+
 client.login(process.env.BOT_TOKEN);
